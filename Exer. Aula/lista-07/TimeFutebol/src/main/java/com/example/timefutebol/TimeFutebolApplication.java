56cc758d2631d96fb14471561627a95a6be697d1
Gabriel Casagrande <126476989+Bielfer12@users.noreply.github.com>
2025-09-19 21:16:25 -0300

    Lista 07 backend

---
+++ Exer. Aula/lista-07/TimeFutebol/src/main/java/com/example/timefutebol/TimeFutebolApplication.java
@@ -0,0 +1,11 @@
+package com.example.timefutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFutebolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeFutebolApplication.class, args);
+    }
+}
