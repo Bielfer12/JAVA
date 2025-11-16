03ed66686f76f1cf2d87233a91f57fb13124bc7a
Gabriel Casagrande <126476989+Bielfer12@users.noreply.github.com>
2025-11-16 00:55:21 -0300

    ApiKey

---
+++ Exer. Aula/LISTA_10/ApiKey/ApiKey/src/main/java/com/example/ApiKey/ApiKeyApplication.java
@@ -0,0 +1,11 @@
+package com.exercicio.apikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeyApplication.class, args);
+    }
+}
